00c5433af3a4e92c8d3b0f3ee8aa40b385e8ad5f
jihanstkm <jihanstk@gmail.com>
2023-05-02 10:07:45 +0600

    use cors and send data to server

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const app = express();
 const cors = require("cors");
 const port = 5001;
 const chef = require("./data/Chef.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(chef);
+});
+
+app.listen(port, () => {
+  console.log("the running port is : ", port);
+});
