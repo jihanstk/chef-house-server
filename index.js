2d3e0948a76548e2816af334636d564ac2ad20b7
jihanstkm <jihanstk@gmail.com>
2023-05-03 13:18:02 +0600

    added single chef

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5001;
-const chef = require("./data/Chef.json");
+const chefs = require("./data/Chef.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(chef);
+  res.send(chefs);
+});
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = chefs.find((c) => c.id == id);
+  res.send(singleChef);
 });
 
 app.listen(port, () => {
